b75148d450f3528a52ea753cdde18114f83b7350
MeseryGamer <meserygamer@outlook.com>
2024-03-04 14:20:25 +0300

    Создан каркас файловой структуры приложения

---
+++ app/src/main/java/com/example/project_artificial_life_androidclient/Contracts/Chat_With_ChatGPT_Contract.java
@@ -0,0 +1,17 @@
+package com.example.project_artificial_life_androidclient.Contracts;
+
+public interface Chat_With_ChatGPT_Contract {
+
+    public interface Model{
+
+    }
+
+    public interface View{
+
+    }
+
+    public interface Presenter{
+
+    }
+
+}
---
+++ app/src/main/java/com/example/project_artificial_life_androidclient/Views/Chat_With_ChatGPT.java
@@ -0,0 +1,16 @@
+package com.example.project_artificial_life_androidclient.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project_artificial_life_androidclient.R;
+
+public class Chat_With_ChatGPT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_with_chatgpt);
+    }
+}
